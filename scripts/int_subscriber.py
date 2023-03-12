836c8adb39b48dcb088380ebeea6d4045ee731e5
GAI-313 <nakatogawagai@gmail.com>
2023-03-12 20:22:11 +0900

    Add Sub adn tx

--- scripts/int_subscriber.py
+++ scripts/int_subscriber.py
@@ -7,6 +7,6 @@ from std_msgs.msg import Int32
 def int_cd(data):
     print(data)
 
-rospy.init_node('int_subscriber')
+rospy.init_node('int_sub')
 rospy.Subscriber('/int_count', Int32, int_cd)
 rospy.spin()
